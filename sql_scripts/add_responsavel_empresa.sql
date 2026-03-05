34c5814e4bf5fa21fadec81cdfb64ee99042eab0
Bruno dos Santos Viana <bruno.viana@hotmail.com>
2026-03-04 23:04:23 -0300

    chore: remove folder sql_scripts and keep consolidated atualizacoes_banco.sql

--- sql_scripts/add_responsavel_empresa.sql
+++
@@ -1,4 +0,0 @@
--- Adiciona colunas responsavel_tecnico e registro_profissional na tabela empresas
-ALTER TABLE public.empresas
-ADD COLUMN IF NOT EXISTS responsavel_tecnico TEXT,
-ADD COLUMN IF NOT EXISTS registro_profissional TEXT;
